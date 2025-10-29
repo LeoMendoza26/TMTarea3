80bb7d1462430acfdbd02b8f5614b1771dcfde0e
Leonel Mendoza <leomendozaher26@gmail.com>
2025-10-29 14:26:34 -0600

    Segundo commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-hola 
+## CICLO WHILE
+
+Realicé algunos ejercicios para entender como funciona el CICLO WHILE
+
+
+## CICLO FOR
+
+Realicé ejercicicos para entender el funcionamiento del CICLO FOR
--- datos.js
+++ datos.js
@@ -1,3 +1,5 @@
+//CICLO WHILE
+
 //ciclo while que cuente del 0 al 5
 let contador = 0;
 
@@ -42,3 +44,47 @@ let numero = 1;
 while (suma < 5) {
      suma += numero; numero++; 
 } console.log(suma);
+
+
+
+//CICLO FOR
+//Ciclo de for que imprime los números del 0 al 9
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+
+//Ciclo de for que calcula la suma del 1 al 10
+let adicion  = 0
+
+for (let i = 1; i <= 10; i++){
+    adicion += i;
+}  
+console.log(adicion);
+
+
+//Programa que cuenta del 10 al 1 con el Ciclo For
+for (let i = 10; i >= 0; i--){
+    console.log(i)
+}
+
+
+//Cilclo de For para que sume los números pares del 1 al 20
+let sumas = 0
+
+for (let i = 1; i <= 20; i++){ 
+    if(i % 2 === 0){
+        sumas += i;
+    }
+}
+console.log("La suma de los números pares del 1 al 20 es:", sumas);
+
+
+for (let i = 0; i < 3; i++) {
+     console.log("Hola"); }
+
+
+let sum = 0;
+ for (let i = 1; i <= 5; i++) { 
+    suma += i; } 
+console.log(sum);
